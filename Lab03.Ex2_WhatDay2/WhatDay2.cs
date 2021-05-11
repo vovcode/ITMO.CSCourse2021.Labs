648e7a8b98bbebc088dcc2e9719ae263b02df7d2
vovcode <cromwell614@gmail.com>
2021-05-12 00:24:31 +0300

    Упражнение 2. Проверка вводимого пользователем значения  дня года

---
+++ Lab03.Ex2_WhatDay2/WhatDay2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03.Ex2_WhatDay2
+{
+    class WhatDay2
+    {
+        enum MonthName
+        {
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Please input a day number between 1 and 365: ");
+                string line = Console.ReadLine();
+                int dayNum = int.Parse(line);
+
+                if ((dayNum < 1) || (dayNum > 365))
+                {
+                    throw new ArgumentOutOfRangeException("Day out of range");
+                }
+
+                int monthNum = 0;
+
+                foreach (int daysInMonth in DaysInMonths)
+                {
+                    if (dayNum <= daysInMonth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        dayNum -= daysInMonth;
+                        monthNum++;
+                    }
+                }
+
+                MonthName temp = (MonthName)monthNum;
+                string monthName = temp.ToString();
+
+                Console.WriteLine("{0} {1}", dayNum, monthName);
+                Console.ReadKey(); //Для удобства проверки
+            }
+            catch (SystemException caught)
+            {
+                Console.WriteLine(caught);
+                Console.ReadKey(); //Для удобства проверки
+            }
+        }
+        // Don't modify anything below here
+        static System.Collections.ICollection DaysInMonths
+            = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    }
+}
