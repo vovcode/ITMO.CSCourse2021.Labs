cb3f51b2ba820d80f28a02d8206b83246ff2a719
vovcode <cromwell614@gmail.com>
2021-05-12 00:01:25 +0300

    Add project files.

---
+++ Lab01.Ex1_Greetings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01.Ex1
+{
+    class Greeter
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            Console.WriteLine("Please enter your name");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello, {0}", myName + "!");//Немножко проявил креативность :)
+            Console.ReadKey();
+        }
+    }
+}
