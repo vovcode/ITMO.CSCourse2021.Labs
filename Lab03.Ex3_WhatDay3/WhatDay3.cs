a002e6fcd1ecf5d91a5ddbe7040998f321a94007
vovcode <cromwell614@gmail.com>
2021-05-12 00:27:13 +0300

    Упражнение 3. Учет високосных годов

---
+++ Lab03.Ex3_WhatDay3/WhatDay3.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03.Ex3_WhatDay3
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+    class WhatDay3
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter the year: ");
+                string line = Console.ReadLine();
+                int yearNum = int.Parse(line);
+                bool isLeapYear = ((yearNum % 4 == 0) && ((yearNum % 100 != 0) || (yearNum % 400 == 0)));
+                //if (isLeapYear)
+                //{
+                //    Console.WriteLine(" IS a leap year");
+                //    Console.ReadLine();
+                //}
+                //else
+                //{
+                //    Console.WriteLine(" is NOT a leap year");
+                //    Console.ReadLine();
+                //}
+
+                int maxDayNum = isLeapYear ? 366 : 365;
+
+
+                Console.Write("Please input a day number between 1 and {0}: ", maxDayNum);
+                line = Console.ReadLine();
+                int dayNum = int.Parse(line);
+                if (dayNum < 1 || dayNum > maxDayNum)
+                {
+                    throw new ArgumentOutOfRangeException("Day out of range");
+                }
+
+                int monthNum = 0;
+
+                if (isLeapYear)
+                {
+                    foreach (int daysInMonth in DaysInLeapMonths)
+                    {
+                        if (dayNum <= daysInMonth)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            dayNum -= daysInMonth;
+                            monthNum++;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (int daysInMonth in DaysInMonths)
+                    {
+                        if (dayNum <= daysInMonth)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            dayNum -= daysInMonth;
+                            monthNum++;
+                        }
+                    }
+                }
+
+
+                MonthName temp = (MonthName)monthNum;
+                string monthName = temp.ToString();
+
+                Console.WriteLine("{0} {1}", dayNum, monthName);
+                Console.ReadKey();
+            }
+            catch (Exception caught)
+            {
+                Console.WriteLine(caught);
+                Console.ReadKey();
+            }
+        }
+        // Don't modify anything below here
+        static System.Collections.ICollection DaysInMonths
+            = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        static System.Collections.ICollection DaysInLeapMonths
+            = new int[12] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    }
+}
