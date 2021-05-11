c5c345f15f301dfeee5803ff80bfa6d5f9c2917a
vovcode <cromwell614@gmail.com>
2021-05-12 00:08:29 +0300

    Упражнение 1. Создание перечисления. 10

---
+++ Lab02.Ex1_BankAccount/Enum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02.Ex1_BankAccount
+{
+    public enum AccountType { Checking, Deposit }
+    class Enum
+    {
+        static void Main(string[] args)
+        {
+            AccountType goldAccount;
+            AccountType platinumAccount;
+
+            goldAccount = AccountType.Checking;
+            platinumAccount = AccountType.Deposit;
+
+
+            Console.WriteLine("The Customer Account Type is {0}", goldAccount);
+            Console.WriteLine("The Customer Account Type is {0}", platinumAccount);
+            Console.ReadKey(); //Для удобной проверки вывода
+
+        }
+    }
+}
