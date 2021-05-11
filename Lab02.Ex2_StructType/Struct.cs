62cd41e821ecd1ae6f2d4c5d44ed64aed67a862c
vovcode <cromwell614@gmail.com>
2021-05-12 00:10:40 +0300

    Упражнение 2. Создание и использование структуры

---
+++ Lab02.Ex2_StructType/Struct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02.Ex2_StructType
+{
+    public enum AccountType { Checking, Deposit }
+    public struct BankAccount
+    {
+        public long accNo;
+        public decimal accBal;
+        public AccountType accType;
+    }
+    class Struct
+    {
+        static void Main(string[] args)
+        {
+            BankAccount goldAccount;
+
+            goldAccount.accType = AccountType.Checking;
+            goldAccount.accBal = (decimal)3200.00;
+            goldAccount.accNo = 123;
+
+            Console.WriteLine("*** Account Summary ***");
+            Console.WriteLine("Acct Number {0}", goldAccount.accNo);
+            Console.WriteLine("Acct Type {0}", goldAccount.accType);
+            Console.WriteLine("Acct Balance ${0}", goldAccount.accBal);
+
+            Console.ReadKey();//Для удобства проверки вывода
+
+        }
+    }
+}
