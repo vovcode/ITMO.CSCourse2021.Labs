31400aacbcc219fe6cebd2b19896e423ae488ad4
vovcode <cromwell614@gmail.com>
2021-05-12 03:09:14 +0300

    Упражнение 3. Использование возвращаемых параметров в методах

---
+++ Lab04.Ex3_Utils/Test.cs
@@ -0,0 +1,52 @@
+﻿namespace Utils
+{
+    using System;
+
+    /// <summary>
+    ///   This is the test harness
+    /// </summary>
+
+    public class Test
+    {
+        public static void Main()
+        {
+            int x;// Значение первого вводимого числа
+            int y;// Значение первого вводимого числа
+            int greater;// Результат из метода Greater()
+
+            Console.WriteLine("Enter the first integer:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second integer:");
+            y = int.Parse(Console.ReadLine());
+            greater = Utils.Greater(x, y);
+
+            Console.WriteLine("The greater value is " + greater);
+            Console.ReadKey();
+
+
+            // Проверка работы метода Swap()
+            Console.WriteLine("Before swap: " + x + "," + y);
+
+            Utils.Swap(ref x, ref y);
+            Console.WriteLine("After swap: " + x + "," + y);
+            Console.ReadKey(); // Для удобства проверки вывода
+
+            // Все что ниже относится к Упражнение 3
+
+            int f; // Factorial result
+            bool ok; // Factorial success or failure
+
+            Console.WriteLine("Number for factorial:");
+            x = int.Parse(Console.ReadLine());
+
+            // Test the factorial function
+            ok = Utils.Factorial(x, out f);
+            // Output factorial results
+            if (ok)
+                Console.WriteLine("Factorial(" + x + ") = " + f);
+            else
+                Console.WriteLine("Cannot compute this factorial");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab04.Ex3_Utils/Utils.cs
@@ -0,0 +1,59 @@
+﻿namespace Utils
+{
+    using System;
+
+    public class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        public static bool Factorial(int n, out int answer)
+        {
+            int k;// Loop counter
+            int f = 1;// Working value
+            bool ok = true; // True if okay, false if not
+
+
+            if (n < 0)
+            {
+                ok = false;
+            }
+
+            try
+            {
+                checked
+                {
+                    f = 1;
+                    for (k = 2; k <= n; ++k)
+                    {
+                        f = f * k;
+                    }
+                }
+            }
+            catch (Exception) // Check for exceptions
+            {
+                f = 0;
+                ok = false;
+            }
+            answer = f;
+            
+            return ok;
+        }
+    }
+}      
