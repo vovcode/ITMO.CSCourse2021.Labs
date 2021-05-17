1279238d9a3424c51903060567c5d2c6c67f2ec8
vovcode <cromwell614@gmail.com>
2021-05-17 17:38:41 +0300

    Упражнение 5. Работа с интерфейсами

---
+++ Lab07.Ex5_TestDisplay/Coordinate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Ex5_TestDisplay
+{
+	// Sample class that implements the IPrintable interface
+
+	class Coordinate : IPrintable
+	{
+		private double x;
+		private double y;
+
+		public Coordinate()
+		{
+			x = 0.0;
+			y = 0.0;
+		}
+
+		public Coordinate(double px, double py)
+		{
+			x = px;
+			y = py;
+		}
+
+		public void Print()
+		{
+			Console.WriteLine("({0},{1})", x, y);
+		}
+	}
+}
---
+++ Lab07.Ex5_TestDisplay/Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Ex5_TestDisplay
+{
+    /// <summary>
+    ///   This the test harness
+    /// </summary>
+
+    public class Test
+    {
+        static void Main()
+        {
+            int num = 65;
+            string msg = "A String";
+            Coordinate c = new Coordinate(21.0, 68.0);
+
+            Utils.Display(num);
+            Utils.Display(msg);
+            Utils.Display(c);
+
+            Console.ReadLine();// Для удобства проверки
+        }
+    }
+
+}
+
+
