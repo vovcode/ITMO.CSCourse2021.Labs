1c2ef48b421e7fb94558ff53e4321d0ec78bfb9a
vovcode <cromwell614@gmail.com>
2021-05-17 10:31:07 +0300

    Упражнение 2. Перемножение матриц

---
+++ Lab05.Ex2_MatrixMultiply/MatrixMultiply.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab05.Ex2_MatrixMultiply
+{
+    class MatrixMultiply
+    {
+        static void Input(int[,] dst)
+        {
+            for (int r = 0; r < 2; r++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    Console.Write("Enter value for [{0},{1}] : ", r, c);
+                    string s = Console.ReadLine();
+                    dst[r, c] = int.Parse(s);
+                }
+            }
+            Console.WriteLine();
+        }
+
+
+        static int[,] Multiply(int[,] a, int[,] b)
+        {
+            int[,] result = new int[2, 2];
+            for (int r = 0; r < 2; r++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    result[r, c] += a[r, 0] * b[0, c] + a[r, 1] * b[1, c];
+                }
+            }
+            return result;
+        }
+
+
+        static void Output(int[,] result)
+        {
+            for (int r = 0; r < result.GetLength(0); r++)
+            {
+                for (int c = 0; c < result.GetLength(1); c++)
+                {
+                    Console.Write("{0} ", result[r, c]);
+                }
+                Console.WriteLine();
+                Console.ReadKey(); //Для удобства проверки выводимых значений
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[,] a = new int[2, 2];
+            int[,] b = new int[2, 2];
+            int[,] result = new int[2, 2];
+
+            MatrixMultiply.Input(a);
+            MatrixMultiply.Input(b);
+
+            result = MatrixMultiply.Multiply(a, b);
+            MatrixMultiply.Output(result);
+        }
+    }
+}
