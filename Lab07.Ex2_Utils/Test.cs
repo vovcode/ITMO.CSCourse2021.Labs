52c0c1056f8bf05b75a9635393e12b237ef9498c
vovcode <cromwell614@gmail.com>
2021-05-17 16:04:53 +0300

    Упражнение 2. Обращение строки

---
+++ Lab07.Ex2_Utils/Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab07.Ex2_Utils
+{
+    public class Test
+    {
+        public static void Main()
+        {
+            string message;
+
+            // Get an input string
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+
+            // Reverse the string
+            Utils.Reverse(ref message);
+
+            // Display the result
+            Console.WriteLine(message);
+
+            Console.ReadLine(); //Для удобства проверки
+
+        }
+    }
+
+}
