f843172690fe5975ec5c8b46670744aa4e519ed5
vovcode <cromwell614@gmail.com>
2021-05-17 13:35:10 +0300

    Упражнение 1. Создание и использование класса

---
+++ Lab06.Ex1_CreateAccount/CreateAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06.Ex1_CreateAccount
+{
+    class CreateAccount
+    {
+        static void Main()
+        {
+            BankAccount berts = NewBankAccount();
+            Write(berts);
+
+            BankAccount freds = NewBankAccount();
+            Write(freds);
+
+            Console.ReadKey(); // Для удобства проверки
+
+        }
+
+        static BankAccount NewBankAccount()
+        {
+            BankAccount created = new BankAccount();
+
+            Console.Write("Enter the account number   : ");
+            long number = long.Parse(Console.ReadLine());
+
+            Console.Write("Enter the account balance! : ");
+            decimal balance = decimal.Parse(Console.ReadLine());
+
+            //created.accNo = number;
+            //created.accBal = balance;
+            //created.accType = AccountType.Checking;
+            created.Populate(number, balance);
+
+            return created;
+        }
+
+        static void Write(BankAccount toWrite)
+        {
+            Console.WriteLine("Account number is {0}", toWrite.Number());
+            Console.WriteLine("Account balance is {0}", toWrite.Balance());
+            Console.WriteLine("Account type is {0}", toWrite.Type());
+        }
+
+    }
+}
