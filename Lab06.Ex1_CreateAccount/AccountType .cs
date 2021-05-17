d182d1e7d6f9ac6ab2870d1d3a5bc3e5f623dc71
vovcode <cromwell614@gmail.com>
2021-05-17 12:46:41 +0300

    Упражнение 1. Создание и использование класса

---
+++ Lab06.Ex1_CreateAccount/AccountType .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab06.Ex1_CreateAccount
+{
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+}
